b58d8cd1aa3aaba6728b2ecfa533f2e5d852ebe2
chenjinpo <chenjinpo6434@gmail.com>
2018-04-13 00:13:09 +0800

    登录@login

--- src/main/java/com/easy/journal/common/controller/LoginController.java
+++ src/main/java/com/easy/journal/common/controller/LoginController.java
@@ -5,9 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
-	@RequestMapping("/login")
+	@RequestMapping("login/login")
 	public String toHello(){
 		
 		return "login/login";
+	}
+	@RequestMapping("login/register")
+	public String toRegister(){
+		
+		return null;
 	}
 }
